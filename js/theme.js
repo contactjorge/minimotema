2791d264fc3836f7eea441e70ca857389e2c8af8
contactjorge <contactjorge@thejorgelopez.com>
2018-05-29 18:29:28 -0500

    Minimo Tema theme  Venus
     A minimial Drupal 8 theme
    
     Now with Bootstrap 4.1
    
     Jorge L Lopez
     IT 390 - Spring 2018 Version - 1805.21
     Copyright 2018
    
     Theme is based on work for IT 390 DePaul and personal portfolio using. Theme is built for Drupal 8 Twig.
    
     This theme is now in version 0.2.0, aka Venus, and utilizes a more advanced subset of features. Regions are now defined closely to actual layout
    
     Merge remote-tracking branch 'origin/master'

--- js/theme.js
+++ js/theme.js
@@ -5,7 +5,20 @@
 /* Jorge L Lopez - IT 390
 /* Version 1805.27
 /* Copyright 2018
-*/
+
+
+var reference = document.querySelector('.my-button');
+var popper = document.querySelector('.my-popper');
+var anotherPopper = new Popper(
+    reference,
+    popper,
+    {
+      // popper options here
+    }
+);
+ */
+
+
 ! function($) {
   $(document).ready(function() {
 
