f4fe59f67c69d1dd4e614b097c63874a8e21da22
contactjorge <contactjorge@thejorgelopez.com>
2018-07-06 15:15:38 -0500

    Minimo Tema theme< Jupiter
    A minimial Drupal 8 TWIG theme
    Now with Bootstrap 4.1
    
    Jorge L Lopez
    Copyright 2018
    
    Theme started as a base theme for work in IT 390 DePaul. Theme and site will be the basis of personal portfolio. Theme is built for Drupal 8 Twig.
    
    This theme is now in version 0.2.0, aka Venus, and utilizes a more advanced subset of features. Regions are now defined closely to actual layout
    
    Merge remote-tracking branch 'origin/master'

--- js/theme.js
+++ js/theme.js
@@ -21,6 +21,12 @@ var anotherPopper = new Popper(
 
 ! function($) {
   $(document).ready(function() {
-
+    $("div#block-minimotema-ctapanel1").addClass("col-md-4");
+    $("div#block-minimotema-ctapanel2").addClass("col-md-4");
+    $("div#block-minimotema-ctapanel3").addClass("col-md-4");
   });
 }(jQuery);
+
+var block1 = document.getElementById("block-minimotema-ctapanel1");
+var block2 = document.getElementById("block-minimotema-ctapanel2");
+var block3 = document.getElementById("block-minimotema-ctapanel3");
